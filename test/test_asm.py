fc014eb1aa0fd4d1988f79a6340ebe11bd6c2528
Manan Shah <manan.shah.777@gmail.com>
2021-03-16 18:44:45 -0700

    Add core contiuous integration framework

---
+++ test/test_asm.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../')
+from features.asm import ASMExtractor
+
+def test_disassemble():
+  extractor = ASMExtractor('data/goodware_example')
+  disassembly = extractor.disassemble()
+  
+  # Make sure that the last disassembly line prints the right pointer
+  assert('0x644a0052013' in disassembly.split('\n')[-2])
