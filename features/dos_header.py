951ea91e80f84fe02b3e76ae02dde5b8938f01b1
Siddhartha Jagannath <kachapah@stanford.edu>
2021-03-31 10:27:59 -0400

    minor fix

--- features/dos_header.py
+++
@@ -1,5 +0,0 @@
-Here's a small example of why you gotta be carefull when bidding your lifesavings away at auctions.  While doing some research for a new purchase of mine (;)) I came
-across this lot at a large auction house.  On the surface it appears to be a platinum AP Tourbillon Automatique avec Reserve de Marche ref 25718PT with a partially complete set.NoneHowever upon
-closer inspection, the tag included has the reference number of a gold (25718BA) reference.  This tells me that someone assembled this set after the fact.  It is not mentioned
-in the lot description as well.  Not a huge deal but a careless bidder could easily spend too much on a watch they thought had a legitimate set.
-It shocks me that auction houses don't pay more attention to these  finer details.
