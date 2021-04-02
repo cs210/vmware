e9345b528ab69b2ad33493d67fb660d26f71cca0
Siddhartha Jagannath <kachapah@stanford.edu>
2021-04-02 15:36:30 -0400

    added ngrams

--- test/test_asm.py
+++ test/test_asm.py
@@ -4,7 +4,7 @@ from features.asm import ASMExtractor
 
 def test_disassemble():
   extractor = ASMExtractor('data/goodware_example')
-  disassembly = extractor.disassemble()
+  disassembly,_ = extractor.disassemble()
   
   # Make sure that the last disassembly line prints the right pointer
   assert('0x644a0052013' in disassembly.split('\n')[-2])
