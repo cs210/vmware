fbea9b7d71da8ab1ec9fbd44fc243fd905f66064
Manan Shah <manan.shah.777@gmail.com>
2021-04-26 23:26:29 -0700

    Add core database code for MySQL meta database and FAISS-backed feature database

--- webapp/backend/database.py
+++ webapp/backend/database.py
@@ -1,6 +1,17 @@
 import faiss
+import numpy as np
+import mysql.connector
+
+import sys
+sys.path.append('../../')
+import features
+import feature_utils
+
 """
-Some thoughts: we may want a mysql meta database consisting of file indexes, hashes, 
+Design thoughts
+---------------
+
+We probably want a mysql meta database consisting of file indexes, hashes, 
 and their corresponding outputs [virustotal + our prediction]. There's also
 potentially space here for user comments and other misc things. We then associate
 the indices in that database with indices in the feature database, which contains
@@ -19,20 +30,80 @@ from scratch. Every time an add operation is performed on the MetaDatabase,
 it's up to the user to make sure there's a corresponding update to the 
 FeatureDatabase. 
 
--- manans@stanford.edu
+-- manans@stanford.edu, April 26 2021
 """
 
 class MetaDatabase:
-  def __init__(self):
-    pass
+  def __init__(self, username="root", password="password", database_name="cw_meta"):
+    # Assumes a mysql setup on localhost with username "root" and password "password"
+    self.database = mysql.connector.connect(
+      host="localhost",
+      user=username,
+      password=password
+    )
 
-  def add(self):
-    pass
+    self._create_database(database_name)
+
+    file_metadata_table_query = (
+      "CREATE TABLE `file_metadata` ("
+      " `id` int(11) NOT NULL AUTO_INCREMENT,"
+      " `md5` char(32) CHARACTER SET 'latin1' NOT NULL,"
+      " PRIMARY KEY (`id`)"
+      ")"
+    )
+    self._create_table("file_metadata", file_metadata_table_query)
+
+    print("INFO: Database setup successful.")
+
+  def _create_database(self, database_name):
+    # Check for the existence of our database, create and warn if doesn't exist
+    cursor = self.database.cursor()
+    cursor.execute("show databases like %s", (database_name, ))
+    rows = cursor.fetchall()
+    
+    if len(rows) == 0: 
+      print("WARNING: Database %s does not exist, creating it." % database_name)
+      cursor.execute("create database %s", (database_name, ))
+    else:
+      print("INFO: Database %s exists." % database_name)
+
+    # Switch to our database
+    cursor.execute("use " + database_name)
+    self.database.commit()
+
+  def _create_table(self, table_name, table_query):
+    # Check for the existence of table_name, create and warn if doesn't exist
+    cursor = self.database.cursor()
+    cursor.execute("show tables like %s", (table_name, ))
+    rows = cursor.fetchall()
+    
+    if len(rows) == 0:
+      print("WARNING: Table %s does not exist, creating it." % table_name)
+      try:
+        cursor.execute(table_query)
+      except mysql.connector.Error as err:
+        print(err.msg)
+    else:
+      print("INFO: Table %s exists." % table_name)
+
+    self.database.commit()
+
+  def add(self, file_md5):
+    cursor = self.database.cursor()
+    insert_query = (
+      "INSERT INTO `file_metadata` VALUES ("
+      "  default, %s"
+      ")"
+    )
+    cursor.execute(insert_query, (file_md5, ))
+    self.database.commit()
+    return cursor.lastrowid if cursor.lastrowid else None
 
 class FeatureDatabase:
   def __init__(self, num_dimensions):
     self.num_dimensions = num_dimensions
     self.index = faiss.IndexFlatL2(num_dimensions)
+    self.index_id_map = faiss.IndexIDMap2(self.index)
   
   """
   Returns the number of elements in the database.
@@ -43,16 +114,24 @@ class FeatureDatabase:
   """
   vectors is expected to be a numpy array with reasonable shape.
   """
-  def add(self, vectors):
-    assert vectors.shape[1] == num_dimensions
-    self.index.add(vectors)
+  def add(self, primary_key, vector):
+    vector = [-1 if feature_utils.is_nan(x) else x for x in vector]
+    primary_key_ = np.array(primary_key).astype('int64').reshape((1, ))
+    vector_ = np.array(vector).astype('float32').reshape((1, -1)) #[:, np.newaxis]
+    self.index_id_map.add_with_ids(vector_, primary_key_)
   
-  def similarity_search(self, vector, k=5):
-    return self.index.search(vector, k)
+  def search_primary_key(self, primary_key):
+    return self.index_id_map.reconstruct(primary_key)
+  
+  def search_similarity(self, vector, k=5):
+    vector = [-1 if feature_utils.is_nan(x) else x for x in vector]
+    vector_ = np.array(vector).astype('float32').reshape((1, -1)) #[:, np.newaxis]
+    return self.index_id_map.search(x=vector_, k=k)
 
   """
   Dumps the content of the database, perhaps something that should
   run in a new process periodically so data is not permanently lost.
   """
   def dump(self, path):
-    faiss.write_index(self.index, path)
+    faiss.write_index(self.index, path + "_index")
+    faiss.write_index(self.index_id_map, path + "_index_id_map")
